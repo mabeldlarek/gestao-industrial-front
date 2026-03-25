4053b1106ec04b675373d8a5c6bd88e365623bb0
Maria Izabel <izabel.162@hotmail.com>
2026-03-25 09:06:51 -0300

    fix/enviroment_corrige ip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://35.199.67.197:8081'
+  apiUrl: 'http://35.247.234.141:8081'
 
 };
