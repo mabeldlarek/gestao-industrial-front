ab1a6a695b1806337eb487e49e58811aa7eb017d
Maria Izabel <izabel.162@hotmail.com>
2026-03-26 11:57:13 -0300

    feat/auth-signin_redireciona tela para login de sucesso e adiciona tratamento de erros de requisição

--- src/app/demo/pages/authentication/auth-signin/auth-signin.component.ts
+++ src/app/demo/pages/authentication/auth-signin/auth-signin.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 import { SharedModule } from 'src/app/theme/shared/shared.module';
@@ -24,11 +24,12 @@ export class AuthSigninComponent {
 
   error = '';
   showPassword = false;
+  private router: Router
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService
-  ) {}
+  ) { }
 
   loginForm = this.fb.group({
     username: ['', [Validators.required]],
@@ -46,9 +47,27 @@ export class AuthSigninComponent {
     this.authService.login(payload).subscribe({
       next: (response) => {
         localStorage.setItem('accessToken', response.accessToken);
+        this.router.navigate(['/dashboard']);
       },
-      error: () => {
-        this.error = 'Usuário ou senha inválidos';
+      error: (err) => {
+        if (err.status === 502) {
+          this.error = 'Servidor indisponível (Bad Gateway). Tente novamente mais tarde.';
+        }
+        if (err.status === 400) {
+          this.error = 'O servidor não entendeu a requisiçãol (Bad Request). Tente novamente mais tarde.';
+        }
+        if (err.status === 401) {
+          this.error = 'Credenciais inválidas. Tente novamente.';
+        }
+        if (err.status === 403) {
+          this.error = 'Acesso não autorizado. Tente novamente.';
+        }
+        if (err.status === 404) {
+          this.error = 'Recurso não encontrado. Tente novamente.';
+        }
+        else {
+          this.error = 'Erro inesperado. Tente novamente mais tarde.';
+        }
       }
     });
   }
